f956ec4f64eb807cddd6737dc655a7d95b6a95d8
jiper <542062320@qq.com>
2018-05-23 20:09:09 +0800

    1、修改宇通客车采销数据入库实现方法
    2、增加数据分析功能

--- Release/YTProductionAndSale/DataToSql.py
+++ Release/YTProductionAndSale/DataToSql.py
@@ -17,36 +17,50 @@
 # 版本缺点
 # 1、依赖在线pdf转excel网站，转换出来的excel可能发生格式错位
 # 2、模拟网页操作，速度相对较慢
+#-------------------------------------------------------------------------------------------
+# 作者：翦林鹏
+# 更新日期：2018-5-15
+# 版本号：V0.2
+# 描述：优化pdf数据获取
+#环境搭建：
+#1、下载pdf2htmlex
+#   下载地址：http://soft.rubypdf.com/software/pdf2htmlex-windows-version，下载后缀为win32-static的版本
+#2、解压pdf2htmlex
+# 使用说明：
+# 1. 将pdf2htmlex解压地址赋值给ExeAdr
+# 2. 确保DownloadAdr下没有任何重要的文件，因为程序会将DownloadAdr文件夹下面的所有文件清空
+# 3. 起始年月默认为2017.6，根据需要修改
+# 4. 输入连接数据库的用户名、密码等，点击运行即可
+
 #--------------------------------------------------------------------
 
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 17:22:21 2018
+
+@author: JianLpeng
+"""
 import pymysql
-import xlrd
-import datetime
+import PdfDown
+import subprocess
+from lxml import etree
 import os
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import datetime
 
 
-class FolderNotCleanException (Exception):
-    pass
-
-class UnfoundExcelFileException (Exception):
-    pass
 
 class ProductionSaleToSql:
-    def ParametersSet(self,user,password,database,stock_code,StockName,DownloadAdr,ExeAdr,YearBegin = 2017,MonthBegin = 6):
+    def __init__(self,user,password,database,stock_code,StockName,ExeAdr=r'E:\JianLPeng\Software\pdfToHtml\pdf2htmlEX.exe',YearBegin = 2017,MonthBegin = 6,DownloadAdr="d:\\downloadTest"):
         self.user = user                      #用户名 
         self.password = password              #密码
         self.database = database              #数据库
         self.stock_code = stock_code          #股票代码
         self.StockName = StockName            #股票名称
         self.DownloadAdr = DownloadAdr        #下载路径
-        self.ExeAdr = ExeAdr                  #可执行文件路径
         self.YearBegin = YearBegin            #起始日期
         self.MonthBegin = MonthBegin          #结束日期
+        self.ExeAdr=ExeAdr
         # 所有的字段列表   
         self.AllField ='''(`stock_code`,`stock_name`,`year`,`month`,`production`,`SPLY_production`,`moth_changeP`,`cumulativeP`,`SPLY_cumulativeP`,`cumulativeP_changeP`,`large_production`,`SPLY_production_large`,\
         `month_changeP_large`,`cumulativeP_large`,`SPLY_cumulativeP_large`,`cumulativeP_changeP_large`,`mid_production`,`SPLY_production_mid`,`month_changeP_mid`,`cumulativeP_mid`,\
@@ -55,27 +69,6 @@ class ProductionSaleToSql:
         `cumulativeS_large`,`SPLY_cumulativeS_large`,`cumulativeS_changeS_large`,`mid_sale`,`SPLY_sale_mid`,`month_changeS_mid`,`cumulativeS_mid`,`SPLY_cumulativeS_mid`,`cumulativeS_changeS_mid`,\
         `small_sale`,`SPLY_sale_small`,`month_changeS_small`,`cumulativeS_small`,`SPLY_cumulativeS_small`,`cumulativeS_changeS_small`)'''
 
-    #清空文件夹
-    def FolderClean(self):
-        for i in os.listdir(self.DownloadAdr):
-           path_file = os.path.join(self.DownloadAdr,i)  # 取文件路径
-           if os.path.isfile(path_file):
-               os.remove(path_file)
-        #文件夹必须清理干净，否则会影响后面的流程
-        if os.listdir(self.DownloadAdr):   #如果文件夹没有清理干净，抛出异常
-            raise FolderNotCleanException
-    
-   #在目录下查询excel文档    
-    def FindExcelFile(self):
-        ExcelPath = ""
-        for i in os.listdir(self.DownloadAdr):
-           path_file = os.path.join(self.DownloadAdr,i)  # 取文件路径
-           if (path_file.find('.xlsx')!=-1 or path_file.find('.xls')!=-1):  #查找后缀为.xlsx或.xls的文件
-               ExcelPath = path_file
-        if ExcelPath:
-            return ExcelPath
-        else:
-            raise UnfoundExcelFileException
       
     #建表
      #命名规则 P：生产量 S：销售量  SPLY：去年同期
@@ -170,187 +163,157 @@ class ProductionSaleToSql:
         cursor.close()
         db.close()
         return flag
-        
-
     
-    #excel文件入库
-    def ExcelToSql(self,ExcelAdr,Year,Month):
-        data = xlrd.open_workbook(ExcelAdr)
-        table =data.sheets()[0]
-        nrows =table.nrows
-        DataInsert = [self.stock_code,self.StockName,Year,Month]
-        for i in range(nrows):
-            if (table.row_values(i)[0]=='生产量'):
-                #测试发现，“生产量”这一列后可能有一行空列
-                if table.row_values(i)[1]:
-                    ColumnAdd=0
-                else:
-                    ColumnAdd = 1 #如果存在空行，则列数加1
-                break;
-        for j in range(i,i+8):
-                DataInsert.extend(table.row_values(j)[1+ColumnAdd:7+ColumnAdd])
-        DataTuple=tuple(list(DataInsert))
-        DataStr = str(tuple(DataTuple))
-        sql = "INSERT INTO `ProductionSale`"+" "+self.AllField+" "+"VALUES"+" "+DataStr
+    
+    
+    def QueryPSData(self,years,months,fieldName):
+       
+        sql = "SELECT "+fieldName+" FROM `ProductionSale` WHERE `stock_code`="+self.stock_code+" AND `year`="+years+" AND `month`="+months
+                 
         db = pymysql.connect(user=self.user,password=self.password,database=self.database,charset="utf8")
         cursor = db.cursor()
         cursor.execute(sql)
-        db.commit()
+        rs=cursor.fetchall()
+        if rs:
+            data=int(rs[0][0].replace(',',''))
+        else:
+            data=-1
+            print ("error! There is no record in the database!")
         cursor.close()
         db.close()
-        
-       
-  
-    
-    
-    
-    #获取最后更新的日期
-    def DateLastUpdate(self,driver):
-        content = driver.find_element_by_xpath("//div[@id='sse_query_list']/dl/dd/a/span").text
-        date = content.split('-')
-        year = date[0]
-        month = date[1]
-        return (year,month)
-    
-    
-    #下载某年某月的产销快报
-    def DownloadPDF(self,driver,year,month):
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("browser.download.folderList", 2)
-        fp.set_preference("browser.download.manager.showWhenStarting", False)
-        fp.set_preference("browser.download.dir", self.DownloadAdr)
-        fp.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
-        fp.set_preference("pdfjs.disabled", True)
-        fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
-        driverPDF = webdriver.Firefox(firefox_profile=fp)
-        
-        
-        XpathTitle =self.StockName+year+"年"+month+"月份产销数据快报"
-        title = "//a[@title=\'"+XpathTitle+"\']"
-        href=driver.find_element_by_xpath(title).get_attribute('href')
-        list=href.split('/')
-        PDF_name=list[-1]
-        
-        driverPDF.get(href)
-        driverPDF.implicitly_wait(10)
-        driverPDF.find_element_by_id("download").click()
-        driverPDF.quit()
-        #driver.back()
-        return PDF_name
-    
-    #PDF转excel
-    def PdfToExcel(self,fileName,driver):
-        #driver.find_element_by_xpath("//div[@class = 'settings']/div[2]/a[2]").click()  #此语句可以
-        driver.find_element_by_id("filePicker").click()
-        fileAdr = self.DownloadAdr+'\\'+fileName
-        os.system(self.ExeAdr+" "+"firefox"+" "+fileAdr)
-        time.sleep(4) 
-        driver.find_element_by_xpath("//div[@class = 'btns']/a").click()
-        #设置显示等待，等待时间不超过50s，每隔0.5检查一次
-        locator = (By.CLASS_NAME, 'downloadBtn')
-        WebDriverWait(driver, 50, 0.5).until(EC.presence_of_element_located(locator))
-        driver.find_element_by_xpath("//div[@class = 'btns']/a[3]").click()
-        driver.refresh()
-    
-    
-      
-    
+        return data
 
+    
+    
+ 
+    def CMDRun(self,cmd):
+        #print('start executing cmd...')
+        s = subprocess.Popen(str(cmd), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        stderrinfo, stdoutinfo = s.communicate()
+        #print('stderrinfo is -------> %s and stdoutinfo is -------> %s' % (stderrinfo, stdoutinfo))
+        #print('finish executing cmd....')
+        return s.returncode
+    
+    def PDF2Html(self,PDFList):
+        print ("Transform PDF to Html...")
+        cmd2 =r' --dest-dir D:\downloadTest\HTML D:/downloadTest/'
+        HtmlList=[]
+        for PDFFile in PDFList:
+            DatePDF=PDFFile.split('_')[-2]
+            year=(int)(DatePDF[0:4])
+            month=(int)(DatePDF[4:6])
+            datePDF =datetime.date(year,month,1)
+            date=datePDF+datetime.timedelta(days = -1)
+            #print (date)
+            if (self.QueryPSTable(str(date.year),str(date.month))==-1):
+                HtmlName= PDFFile.split('.')[0]+'.html'
+                CMD=self.ExeAdr+cmd2+PDFFile+' '+HtmlName
+                print ("transform "+PDFFile)
+                self.CMDRun(CMD)
+                HtmlList.append(HtmlName)
+                print ('done')
+            else:
+                print(self.StockName+str(date.year)+'年'+str(date.month)+'月已有记录')       
+        print ("Transform done!")
+        #print (HtmlList)
+        return HtmlList
+        
+          
+    def HtmlScrap(self,HtmlList):
+        for Htmlname in HtmlList:
+            HtmlFolder = os.path.join(self.DownloadAdr,'HTML')
+            HtmlPath=os.path.join(HtmlFolder,Htmlname)
+            
+            YMD=Htmlname.split('_')[-2]
+        
+            month=YMD[4:6]
+            if (month[0]=='0'):
+                month=month[1:]
+            
+            year=YMD[0:4]
+            dateHtml=datetime.date(int(year),int(month),1)
+            date=dateHtml+datetime.timedelta(days = -1)
+            htmlf=open(HtmlPath,'r',encoding="utf-8")
+            html=htmlf.read()
+            selector=etree.HTML(html)
+            #element='//*[@id="pf1"]/div[1]/div[21]/div'
+            lls=[self.stock_code,self.StockName]
+
+#            if ((selector.xpath('//*[@id="pf1"]/div[1]/div[3]/text()')[0])=='证券代码：'):
+#                offset=20
+#
+#            else:
+#                offset=18
+          
+            try:
+                path1='//*[@id="pf1"]/div[1]/div['
+                path2=']/div/text()' 
+                for i in range(0,100):
+                    LocPath=path1+str(i)+path2
+                    LocElement=selector.xpath(LocPath)
+                   
+                    if (LocElement!=[] and LocElement[0]=='生产量' ):
+                        offset=i
+                        break
+                print (offset)
+             
+                year =str(date.year)
+                month = str(date.month)
+                lls.append(str(date.year))
+                lls.append(str(date.month))
+                for i in range(0,8):
+                    for j in range(1,7):
+                        num=str(j+i*7+offset)
+                        path=path1+num+path2
+                        content=selector.xpath(path)
+                        #print(content)
+                        lls.append(content[0])
+                #print (lls)
+                DataTuple=tuple(list(lls))
+                DataStr = str(tuple(DataTuple))
+                sql = "INSERT INTO `ProductionSale`"+" "+self.AllField+" "+"VALUES"+" "+DataStr
+                db = pymysql.connect(user=self.user,password=self.password,database=self.database,charset="utf8")
+                cursor = db.cursor()
+                cursor.execute(sql)
+                db.commit()
+                cursor.close()
+                db.close()
+                print(self.StockName+year+'年'+month+'月入库成功')
+            except:
+                 print(self.StockName+year+'年'+month+'月入库失败')
+            finally:
+                htmlf.close()
+            
+
+      
     def ProSaleUpdate(self):
         self.CreatePSTable()
-        #配置产销快报页面参数，实现pdf自动下载  
-        self.CreatePSTable()
-        try:
-            fp = webdriver.FirefoxProfile()
-            fp.set_preference("browser.download.folderList", 2)
-            fp.set_preference("browser.download.manager.showWhenStarting", False)
-            fp.set_preference("browser.download.dir", self.DownloadAdr)
-            fp.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
-            fp.set_preference("pdfjs.disabled", True)
-            fp.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
-            driver = webdriver.Firefox(firefox_profile=fp)
-            #driver.maximize_window()
-            #打开页面
-            url = "http://www.sse.com.cn/home/search/?webswd="+self.StockName+"产销快报"
-            driver.get(url)
-        
-            
-            #配置pdf转excel页面参数，实现excel文件自动下载
-            profile = webdriver.FirefoxProfile()
-            profile.set_preference('browser.download.dir', self.DownloadAdr)
-            profile.set_preference('browser.download.folderList', 2)
-            profile.set_preference('browser.download.manager.showWhenStarting', False)
-            #profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/vnd.ms-excel')
-            profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet ')
-            driverEx = webdriver.Firefox(firefox_profile=profile)
-            #打开页面
-            urlEx = "http://app.xunjiepdf.com/pdf2excel"
-            driverEx.get(urlEx)
-            #driverEx.implicitly_wait(4)
-        
-            #设置隐式等待，括号中的参数为最大等待时间，如果在最大等待时间内，页面加载完成，则等待立结束，否则抛出异常
-            #整个driver的时间内，该设置只用设置一次
-            driver.implicitly_wait(100)
-            driverEx.implicitly_wait(50)
-            
-            #从产销快报页面获取最新一次更新的日期，月份-1作为获取数据的起始年、月参数
-            (year,month)=self.DateLastUpdate(driver)
-            if (month=="1"):
-                YearInt = int(year)-1
-                MonthInt= 12
-            else:
-                YearInt = int(year)
-                MonthInt= int(month) -1       
-        
-            #清空下载文件夹，避免历史文件的干扰
-            self.FolderClean()
-            DateBegin =datetime.date(YearInt,MonthInt,1)
-            DateEnd = datetime.date(self.YearBegin,self.MonthBegin,1)
-            #count用于计数，因为一个页面只有10条数据，所以每隔10条需要翻页
-            count = 0
-            for i in range(0,(DateBegin-DateEnd).days+1,1):
-                day = DateBegin -datetime.timedelta(days=i)
-                if (day.day == 1):
-                    count = count+1
-                    YearExact = day.year
-                    MonthExact = day.month
-                    try:
-                        if(count==11):
-                            driver.find_element_by_id('Next').click()
-                            count=0
-                        TrueOrFalse = self.QueryPSTable(years=str(YearExact),months=str(MonthExact))
-                        if (TrueOrFalse == -1):
-                            PDF_Name = self.DownloadPDF(driver=driver,year=str(YearExact),month=str(MonthExact))
-                            self.PdfToExcel(fileName=PDF_Name,driver = driverEx)
-                            ExcelAdr = self.FindExcelFile()
-                            self.ExcelToSql(ExcelAdr=ExcelAdr,Year=str(YearExact),Month=str(MonthExact))         
-                            print (self.StockName+str(YearExact)+"年"+str(MonthExact)+"月入库成功")
-                        else:
-                            print (self.StockName+str(YearExact)+"年"+str(MonthExact)+"月数据库中已有记录")
-                        self.FolderClean()
-                    except (Exception) as e:
-                        print (self.StockName+str(YearExact)+"年"+str(MonthExact)+"月入库失败，请检查")
-                        print ("检查失败原因："+str(e))
-                else:
-                    pass
-        finally:
-            driver.quit()
-            driverEx.quit()
-            self.FolderClean()
+        downLoad = PdfDown.PdfDownLoad(self.YearBegin,self.MonthBegin)
+        downLoad.GetAllPdfFile()
+        #print (downLoad.pdfList)
+        HtmlList=self.PDF2Html(downLoad.pdfList)
+        self.HtmlScrap(HtmlList)
+#        TxtTrans = PDF_TXT.PDFToTXT(PDFList=downLoad.pdfList)
+#        TxtTrans.TransAll()
+#        for Txt in TxtTrans.TxtList:
+#            txtAdd=self.DownloadAdr+'/'+Txt
+#            self.TxtToSql(txtAdd)
+#            print (Txt+'入库成功')
             
      
 if __name__ == "__main__":
     user = "root"
-    password = ""
-    database = ""
+    password = "jip6669635"
+    database = "db_test1"
     stock_code = "600066"
     StockName = "宇通客车"
+    ExeAdr=r"E:\JianLPeng\Software\pdfToHtml\pdf2htmlEX.exe"
     DownloadAdr = "d:\\downloadTest"
-    #ExeAdr=r"E:\JianLPeng\Project\Anack\anack\Release\YTProductionSaleSql\UpfileWithPara.exe" #绝对路径
-    ExeAdr="UpfileWithPara.exe"   #如果UpfileWithPara.exe在同一个文件夹下可以使用相对路径
-    Update = ProductionSaleToSql()
-    Update.ParametersSet(user=user,password=password,database=database,stock_code=stock_code,StockName=StockName,DownloadAdr=DownloadAdr,ExeAdr=ExeAdr,YearBegin = 2017,MonthBegin = 6)
+    Update = ProductionSaleToSql(user=user,password=password,database=database,stock_code=stock_code,ExeAdr=ExeAdr,StockName=StockName,DownloadAdr=DownloadAdr,YearBegin = 2016,MonthBegin = 9)
+   # Update.ParametersSet(user=user,password=password,database=database,stock_code=stock_code,StockName=StockName,DownloadAdr=DownloadAdr,ExeAdr=ExeAdr,YearBegin = 2015,MonthBegin = 9)
     Update.ProSaleUpdate()
+#    data=Update.QueryPSData("2017","1","production")
+#    print (data)
     
 """
 self.user = user                      #用户名 
@@ -362,6 +325,5 @@ self.DownloadAdr = DownloadAdr        #下载路径
 self.ExeAdr = ExeAdr                  #可执行文件路径
 self.YearBegin = YearBegin            #起始日期
 self.MonthBegin = MonthBegin          #结束日期
+日期要大于2015年8月（因为这之前的格式会发生变化）
 """
-
-
