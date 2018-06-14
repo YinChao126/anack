cf1c9cb885e325902d43062f9bd1dedb5d946e0e
YinChao126 <yc86247931@126.com>
2018-06-15 02:23:46 +0800

    完善了para_list的定义，可进行协同开发了

--- M1809/yc/GetItemInfo.py
+++ M1809/yc/GetItemInfo.py
@@ -16,102 +16,100 @@ import Config
 
 '''
 list 对照表
-0 总资产
-1 净资产
-2 流动资产
-3 现金
-4 一年内到期的长期负债
-5 资产负债比
-6 应收账款
-7 预收账款
-8 研发投入比
-9 流动比率
-10 资产周转率
-11 存货周转率
-12 现金增长净额
-13 营收总额
-14 支出总额
-15 净利润
-16 除非净利润
-17 经营所得税
+0   总资产
+1   净资产
+2   资产负债比
+3   流动资产
+4   一年内到期的长期负债
+5   应收账款
+6   预收账款
+7   存货
 
+8   营业收入
+9   营业成本
+10  营业税金及附加
+11  财务费用
+12  营业外收入
+13  净利润
+14  除非净利润
+15  每股收益
+
+16  经营净额
+17  投资净额
+18  筹资净额
+19  汇率影响
+20  现金净增加额
+21  期末现金余额
+
+22  流动比率
+23  资产周转率
+24  存货周转率
+
+25  总市值/净资
+26  市盈率
+27  市净率
+28  名义净资产收益率
+29  真实净资产收益率
+30  毛利率
+31  营收增长率
+32  除非净利润增长率
 '''
-def GetAllData(para, stock_id, year):
+def GetSingleItem(para, stock_id, year):
     '''
     返回一个series，
     '''
+    
+    #
+    p_len = len(para) #自动计算参数列表长度
+    info = []   #实际待填充的字段，最后用于生成Series的value部分
+  
+    for s in range(p_len):
+        info.append(-1) #初始化为-1，代表还未填充
+        
     cur = Config.Connect_sql()
+    
+    #资产负债表中查询与填充
     cmd = "select * from zichanfuzai where h79 = \'"+stock_id+"\' and h80 = \'2017-12-31\';"
     cur.execute(cmd)
     result = cur.fetchall()
     data = result[0] #获得资产负债表信息
-    p_len = len(para)
     
-    info = []
-    #此段用以实现result的字段填充，如需修改，请修改此处
-    for s in range(p_len):
-        info.append(-1)
+    info[0] = float(data[37].replace(',','')) #总资产
+    debt = float(data[65].replace(',','')) #总负债
+    info[2] = round((debt / info[0]),2)     #资产负债比
+    info[1] = info[0] - debt #净资产
+    info[3] = float(data[16].replace(',','')) #流动资产
+    info[4] = float(data[52].replace(',','')) #一年内到期的长期负债
+    info[5] = float(data[4].replace(',',''))  #应收账款
+    info[6] = float(data[42].replace(',',''))  #预收账款
+    info[7] = float(data[10].replace(',',''))  #存货
     
-    info[0] = float(data[37].replace(',',''))
-    info[2] = float(data[16].replace(',',''))
-    info[3] = float(data[0].replace(',',''))
-    info[4] = float(data[12].replace(',',''))
-    info[5] = float(data[65].replace(',',''))
-    info[5] = round((info[5] / info[0]),2)
-    info[6] = float(data[4].replace(',',''))
-    
+    #利润表查询与填充
     cmd = "select * from Profit where h29 = \'"+stock_id+"\' and h30 = \'2017-12-31\';"
     cur.execute(cmd)
     result = cur.fetchall()
     data = result[0] #获得资产负债表信息
 #    print(data)
-    info[13] = float(data[0].replace(',','')) #营收总额
-    info[14] = float(data[2].replace(',','')) #费用总额
-    info[15] = float(data[19].replace(',','')) #净利润
-    info[17] = float(data[4].replace(',','')) #所得税
+    info[8] = float(data[1].replace(',','')) 
+    info[9] = float(data[3].replace(',',''))  
+    
 
+    #现金流量表查询与填充
     cmd = "select * from cashFlow where h72 = \'"+stock_id+"\' and h73 = \'2017-12-31\';"
     cur.execute(cmd)
     result = cur.fetchall()
     data = result[0] #获得资产负债表信息
-#    print(data)
-    info[12] = float(data[70].replace(',','')) #现金增长净额
-#    info[3] = float(data[66].replace(',','')) #现金余额
+    
         
+    #指标计算，可能还需要获取额外的数据如实时股价等。。。
+    info[8] = float(data[1].replace(',','')) 
+    
+    
     return pd.Series(info,index = para)
 
 
-
-
-
-
-
-
-
-
-def GetSingleInfo(para, name, year):
-    '''
-    返回一个series，
-    '''
-    cur = Config.Connect_sql()
-    cmd = "select * from anack_classify where name = \'"+name+"\';"
-    cur.execute(cmd)
-    result = cur.fetchall()
-    id = result[0][0] 
-    print(id)
-    p_len = len(para)
-    
-    result = []
-    #此段用以实现result的字段填充，如需修改，请修改此处
-    for s in range(p_len):
-        result.append(s)
-        
-    return pd.Series(result,index = para)
-
-##############################################################################
-#GetSingleInfo(parameter,'隆基股份',2016)
-#print(company_id)
+###############################################################################
 if __name__ =='__main__':
     parameter,company_id = Config.M1809_config() #获取配置信息
-    s = GetAllData(parameter,company_id[0],2017)
+    s = GetSingleItem(parameter,company_id[0],2017)
     print(s)
